10f6bedb63c1108fc42697ff056e4ec9c610658a
castillo05 <jorgeantonio20142014@gmail.com>
2020-03-30 22:03:15 -0600

    Confirmando email y redirigiendo al login

--- src/views/pages/Confirm.js
+++ src/views/pages/Confirm.js
@@ -6,18 +6,24 @@ import { Alert } from 'reactstrap';
 
 
 const Confirm = (props) => {
+   
     // console.log(props.match.params.email)
     const email=props.match.params.email;
-    const [data,setData]=useState({form:{email:''}})
+    const form={email:email}
     const [url, setUrl] = useState('https://localhost:44354/api/Auth/register/confirm');
     const [alert,setAlert]=useState({status:false,text:'',description:'',type:''})
 
     const postConfirm=async()=>{
+        
         try {
-             setData({form:email})
-            const req=await axios.post(url,data.form.email,{
-
+            
+            const req=await axios.post(url,JSON.parse(JSON.stringify(form)),{
+                headers:{'Content-Type':'application/json'}
             });
+            if(req.data.message){
+                return setAlert({status:true,text:'Error!',description:req.data.message,type:'danger'})
+            }
+            props.history.push('/auth/login')
             console.log(req)
         } catch (error) {
             setAlert({status:true,text:'Error!',description:'Disculpa al parecer a ocurrido un error al confirmar tu correo electronico, por favor ponte en contacto con nosotros lo antes posible Gracias...!',type:'danger'})
@@ -27,10 +33,9 @@ const Confirm = (props) => {
     }
 
     useEffect(() => {
+       
         postConfirm();
-        return () => {
-            
-        };
+       
     }, []);
     return (
         <>
